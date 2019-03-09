46bcfd3203f6fb12a8498328e5373ec62c3caf20
Rudolf Cicko <rudolfcicko2016@gmail.com>
2019-03-09 18:30:07 +0100

    Adding another source for random phrases

--- TextToSpeechDownload/automatizer/index.js
+++ TextToSpeechDownload/automatizer/index.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const fs = require('fs');
 const exec = require('child_process').exec;
-const debounce = require('debounce');
+const debounce = require('debounce-promise');
 
 function chunkString(str, length) {
   return str.match(new RegExp('.{1,' + length + '}', 'g'));
@@ -9,42 +9,94 @@ function chunkString(str, length) {
 
 let mapContent = '';
 
+const promiseSerial = funcs =>
+  funcs.reduce((promise, func) =>
+    promise.then(result => func().then(Array.prototype.concat.bind(result))),
+    Promise.resolve([]));
+
+function concatToMapContent(phrase) {
+  console.log('CONCATENATING TO THE MAP.TXT FILE');
+  mapContent += `${phrase.split(' ').join('_')}.wav\t${phrase}\n`;
+}
+
+function saveMapContentFile() {
+  fs.writeFile("map.txt", mapContent, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+  });
+}
+
 function getPromiseForPhrase(phrase) {
-  new Promise((resolve, reject) => {
-    debounce(exec('python tts.py "'.concat(phrase).concat('"'), (error, stdout, stderr) => {
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-      resolve();
-      if (error !== null) {
-        reject(error);
-          console.log(`exec error: ${error}`);
+  return () => new Promise((resolve, reject) => {
+    exec('python tts.py "'.concat(phrase).concat('"'), (error, stdout, stderr) => {
+      if (stdout.includes('TTS') && !stderr && !error) {
+        console.log(`stdout: ${stdout}`);
+        concatToMapContent(phrase);
       }
-    }), 500);
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+      } else if (error !== null) {
+        console.log(`exec error: ${error}`);
+      }
+      resolve();
+    });
+  });
+}
+
+function getRandomWordGeneratorPhrases() {
+  axios.get('https://randomwordgenerator.com/json/sentences.json').then((res) => {
+    const promises = [];
+    res.data.data.forEach((phrase, index) => {
+      /*
+      if (phrase.sentence.length > 25) {
+        phrase.sentence.split(' ').forEach(smPhrase => {
+          promises.push(getPromiseForPhrase(smPhrase));
+        });
+      } else {
+        */
+        promises.push(getPromiseForPhrase(phrase.sentence));
+      // }
+    });
+    promiseSerial(promises).then(() => {
+      console.log('DONE');
+      saveMapContentFile();
+    })
+  });
+}
+
+const isQuestion = phrase => phrase.includes('?');
+
+function getPhrasesByOneByOne() {
+  const N = 100;
+  let promises = [];
+  for(let i = 0; i < N; i++) {
+    promises.push(() => new Promise((resolve, reject) => {
+      axios.post('http://watchout4snakes.com/wo4snakes/Random/NewRandomSentence').then((res) => {
+        const phrase = res.data;
+        if (!isQuestion(phrase)) {
+          getPromiseForPhrase(phrase)()
+          .then(() => {
+            saveMapContentFile();
+            resolve();
+          })
+          .catch(reject);
+        } else {
+          resolve();
+        }
+      });
+    }));
+  }
+  promiseSerial(promises)
+  .then(() => {
+    console.log('DONE');
+    saveMapContentFile();
+  })
+  .catch(e => {
+    console.log('ERROR');
+    console.log(e);
   })
 }
 
-axios.get(`https://randomwordgenerator.com/json/sentences.json?cucu=${Math.random()}`).then((res) => {
-  const promises = [];
-  res.data.data.forEach(phrase => {
-    /*
-    if (phrase.sentence.length > 25) {
-      phrase.sentence.split(' ').forEach(smPhrase => {
-        promises.push(getPromiseForPhrase(smPhrase));
-      });
-    } else {
-      */
-    mapContent += `${phrase.sentence.split(' ').join('_')}.wav ${phrase.sentence}\n`;
-    promises.push(getPromiseForPhrase(phrase.sentence));
-    // }
-  });
-  Promise.all(promises).then(() => {
-    console.log('DONE');
-    console.log(mapContent.length);
-    fs.writeFile("map.txt", mapContent, function(err) {
-      if(err) {
-          return console.log(err);
-      }
-      console.log("The file was saved!");
-    });
-  })
-});
+getPhrasesByOneByOne();
